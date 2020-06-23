252a12e4261f1122be21a2bc4c08c313f868633d
Stephen Pittman <stephen@gomerits.com>
2020-06-23 11:50:01 +1000

    Add sounds.sh script

---
+++ sounds_post.sh
@@ -0,0 +1,4 @@
+cp -r ../damalsksoundpack/ data/sound/
+rm -rf data/sound/damalsksoundpack/music
+cp -r ~/coag/CONTENTS/music/ data/sound/damalsksoundpack/
+cp ~/coag/CONTENTS/musicset.json data/sound/damalsksoundpack/music
